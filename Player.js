d8edacc2b24e2af31bc673fac74cae89351fc118
Sebastianpleygames <sebastianpleygames.dk@gmail.com>
2023-09-21 08:49:59 +0200

    adding player

---
+++ Player.js
@@ -0,0 +1,114 @@
+class Player{
+    constructor(x, y){
+     this.pos = createVector(x,y);
+     this.dir = createVector(1,0);
+     this.angle = 0;
+     this.vel = createVector(0,0); 
+  
+      
+      console.log(this.vel.heading());
+      
+  
+    }  
+    
+      show(){
+      
+    push();
+      stroke(10,10,255);
+      beginShape();
+      translate(this.pos.x,this.pos.y);
+      rotate(this.angle);
+      
+      vertex(16, 0);
+      vertex(-12, -10);
+      vertex(-8, 0);
+      vertex(-12, 10);
+      vertex(16, 0);
+        
+      endShape();
+      
+    if(keyIsDown(UP_ARROW)){
+      stroke(240,127,19);
+      beginShape();
+      vertex(-10, 0);
+      vertex(-14, 1);
+      vertex(-18, 0);
+      vertex(-14, -1);
+      vertex(-10, 0);
+      
+      endShape();
+      
+      
+      
+      
+    }
+      pop();     
+  }
+    
+     
+    
+    
+     update() {
+  
+       this.pos.add(this.vel);
+       
+       if (this.pos.x < -10){
+  this.pos.x = width;       
+  
+       }
+      
+      if (this.pos.x > width+10){
+  this.pos.x = 0;   
+  
+       }
+       
+      if (this.pos.y < -10){
+  this.pos.y = height;       
+  
+       }
+      if (this.pos.y > height+10){
+  this.pos.y = 0;       
+  
+       }
+       
+     //   console.log(mouseX,mouseY)
+       
+       
+       if (keyIsDown(RIGHT_ARROW)){
+         
+         this.angle +=2
+         this.dir.rotate(2);
+         
+       }
+      if (keyIsDown(LEFT_ARROW)){
+         
+         this.angle -=2
+         this.dir.rotate(-2);
+         
+       } 
+      
+      if (keyIsDown(UP_ARROW)){
+         
+      this.dir.setMag(0.05);
+      this.vel.add(this.dir);
+      this.vel.limit(5);
+      
+         
+       }
+        if (!keyIsDown(UP_ARROW)){
+         
+      this.vel.mult(0.99);
+    
+        
+          
+       }
+       
+     
+    }  
+  }
+  
+  
+  
+  
+  
+  //push pop betyder at translate kun virker i den ene instance 
--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  angleMode(DEGREES);
+  createCanvas(400, 600);
+  
+
+  Player = new Player(width/2,height/2);  
+
+
 }
 
 function draw() {
-  background(220);
+ background(220);
   
-  rect(width/2,20,20,20);
+  rect(width/2,height/2,20,20);
   circle(mouseX,mouseY,20);
+
+  Player.update();
+  Player.show();
+
 }
