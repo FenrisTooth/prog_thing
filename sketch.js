0fc48c96747898cff2c97443ea5c52ce4587336b
Sebastianpleygames <sebastianpleygames.dk@gmail.com>
2023-09-21 10:07:58 +0200

    test

--- sketch.js
+++ sketch.js
@@ -6,10 +6,7 @@ function setup() {
 
   Player = new Player(width/2,height/2);  
 
-  for (let i = 0; i < random(10,20); i++) {
-  
-    asteroids1.push(new asteroids());
-   }
+
 }
 
 function draw() {
@@ -21,14 +18,5 @@ function draw() {
   Player.update();
   Player.show();
 
-<<<<<<< HEAD
-=======
-  for (let i = 0; i < asteroids1.length; i++){
-    
-    asteroids1[i].update();
-    asteroids1[i].show();
-      
-     }
->>>>>>> main
 
 }
